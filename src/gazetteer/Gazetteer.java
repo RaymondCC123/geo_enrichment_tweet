d5b6702c8b26d441721c0bc25b18b7585703bf88
Raymond Cheng CAO <caocheng1994@gmail.com>
2018-06-29 12:25:21 +0200

    liste des emplacements

---
+++ src/gazetteer/Gazetteer.java
@@ -0,0 +1,82 @@
+/**
+ * 
+ */
+package gazetteer;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.TreeMap;
+
+
+
+/**
+ * A gazetteer that stores information about a list of locations
+ *
+ */
+public class Gazetteer {
+	public final Hashtable<String, List<Location>> locations = new Hashtable<String, List<Location>>();
+	public final Hashtable<String, Location> ids = new Hashtable<String, Location>();
+	
+	/**
+	 * @param fileName the directory to gazetteer file(i.e. hyer.txt)
+	 * @throws IOException
+	 */
+	public Gazetteer(String fileName) throws IOException {
+		InputStream fis = new FileInputStream(fileName);
+		BufferedReader br = new BufferedReader(new InputStreamReader(fis, Charset.forName("UTF-8")));
+		String line;
+		while ((line = br.readLine()) != null) {
+		    // Deal with the line
+			String[] parts = line.replace("\n", "").split("\t", 8);
+			Location temp = new Location(parts[0], parts[1], parts[2], parts[3],
+					parts[4], parts[5], parts[6]);
+			ids.put(temp.id, temp);
+			if (!locations.containsKey(temp.name.toLowerCase())) {
+			    locations.put(temp.name.toLowerCase(), new ArrayList<Location>(Arrays.asList(temp)));
+			}
+			else if (!locations.get(temp.name.toLowerCase()).contains(temp)){
+				locations.get(temp.name.toLowerCase()).add(temp);
+			}
+			// add hashtable for alternate names
+			for (String name: parts[7].split(",")){
+				if (!locations.containsKey(name.toLowerCase())) {
+				    locations.put(name.toLowerCase(), new ArrayList<Location>(Arrays.asList(temp)));
+				}
+				else if (!locations.get(name.toLowerCase()).contains(temp)){
+					locations.get(name.toLowerCase()).add(temp);
+				}
+			}
+		}
+		// Done with the file
+		br.close();
+		br = null;
+		fis = null;
+	}
+	/**
+	 * Return a List of Locations matching a given name
+	 * 
+	 * @param name
+	 * @return a List of Locations
+	 */
+	public List<Location> get(String name){
+		return locations.get(name.toLowerCase());
+	}
+	/**
+	 * Return a Location matching a given id
+	 * 
+	 * @param id
+	 * @return a Location
+	 */
+	public Location getByID(String id) {
+		return ids.get(id);
+	}
+	
+}
---
+++ src/gazetteer/Location.java
@@ -0,0 +1,83 @@
+/**
+ *
+ */
+package gazetteer;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * the class that stores information of a certain location
+ */
+public class Location implements Comparable<Location> {
+    /**
+     * a unique identifier of this location
+     */
+    public final String id;
+    /**
+     * the official name of this location
+     */
+    public final String name;
+    /**
+     * the type of this location; can be either "city", "SP", "country" or "continent"
+     */
+    public final String type;
+    /**
+     * latitude of this location; only available for cities.
+     */
+    public final String lat;
+    /**
+     * longitude of this location; only available for cities.
+     */
+    public final String lon;
+    /**
+     * population of this location; only available for cities.
+     */
+    public final int population;
+    /**
+     * higher political districts of this location.
+     */
+    public final List<String> higher;
+
+    public Location(String id, String name, String type, String higher, String population,
+                    String lat, String lon) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.higher = Arrays.asList(higher.split("_"));
+        int temp;
+        try {
+            temp = Integer.parseInt(population);
+        } catch (NumberFormatException e) {
+            temp = 0;
+        }
+        this.population = temp;
+        this.lat = lat;
+        this.lon = lon;
+        //this.alternate = Arrays.asList(parts[7].split(","));
+    }
+
+    @Override
+    public int compareTo(Location that) {
+        return this.population - that.population;
+    }
+
+    /**
+     * Calculate distance between two locations, based on their coordinates.
+     *
+     * @param that the other location
+     * @return distance measured by KM; Double.MAX_VALUE if unable to calculate
+     */
+    public double distanceTo(Location that) {
+        try {
+            double a = Math.sin(Math.toRadians((Double.parseDouble(this.lat) - Double.parseDouble(that.lat)) / 2))
+                    * Math.sin(Math.toRadians((Double.parseDouble(this.lat) - Double.parseDouble(that.lat)) / 2))
+                    + Math.cos(Math.toRadians(Double.parseDouble(this.lat))) * Math.cos(Math.toRadians(Double.parseDouble(that.lat)))
+                    * Math.sin(Math.toRadians((Double.parseDouble(this.lon) - Double.parseDouble(that.lon)) / 2))
+                    * Math.sin(Math.toRadians((Double.parseDouble(this.lon) - Double.parseDouble(that.lon)) / 2));
+            return 6371 * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        } catch (NumberFormatException e) {
+            return Double.MAX_VALUE;
+        }
+    }
+}
